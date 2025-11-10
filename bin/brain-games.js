02a38b4432f5bab364f17208d9b5ee17daba5726
Роберт <prokotrob@gmail.com>
2025-11-10 14:43:08 +0700

    Сделал файл,при запуске пишет приветствие

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
-chmod +x bin/brain-games.js
+import { greetUser } from '../src/cli.js';
+
+greetUser();
 
