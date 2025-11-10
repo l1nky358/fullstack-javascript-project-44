ce70b59c268f4798d0c4d01e012242d880acb962
l1nky358 <prokotrob@gmail.com>
2025-11-10 14:54:34 +0700

    Добавлена игра calc, центральный модуль index.js, обновлен package.json"

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,59 @@
-#!/usr/bin/env node
-const runGame = require('../src');
-const getGame = require('../src/games/brain-calc');
+import readlineSync from 'readline-sync';
 
-runGame('brain-calc', getGame);
-chmod +x bin/brain-calc.js
+const roundsCount = 3;
+
+const getUserName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+const runGame = (gameName) => {
+  const userName = getUserName();
+
+  const gameFunctions = {
+    calc: () => {
+      for (let i = 0; i < roundsCount; i += 1) {
+        const num1 = Math.floor(Math.random() * 100);
+        const num2 = Math.floor(Math.random() * 100);
+        const operations = ['+', '-', '*'];
+        const operation = operations[Math.floor(Math.random() * operations.length)];
+        let correctAnswer;
+
+        switch (operation) {
+          case '+':
+            correctAnswer = num1 + num2;
+            break;
+          case '-':
+            correctAnswer = num1 - num2;
+            break;
+          case '*':
+            correctAnswer = num1 * num2;
+            break;
+        }
+
+        console.log(`Question: ${num1} ${operation} ${num2}`);
+        const answer = readlineSync.question('Your answer: ');
+
+        if (Number(answer) !== correctAnswer) {
+          console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+          console.log(`Let's try again, ${userName}!`);
+          return;
+        } else {
+          console.log('Correct!');
+        }
+      }
+      console.log(`Congratulations, ${userName}!`);
+    },
+   
+  };
+
+  if (gameFunctions[gameName]) {
+    gameFunctions[gameName]();
+  } else {
+    console.log('Unknown game');
+  }
+};
+
+export { startGame };
