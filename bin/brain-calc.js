74e1534f17d491c01591ded8d6a37fce7a6909c5
l1nky358 <prokotrob@gmail.com>
2025-11-11 16:47:57 +0700

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
 function startGame() {
-
   const number1 = Math.floor(Math.random() * 100);
   const number2 = Math.floor(Math.random() * 100);
   const operations = ['+', '-', '*'];
   const operation = operations[Math.floor(Math.random() * operations.length)];
 
-  const question ='Question: <number1> <operation> <number2>';
-console.log (Question: <number1> <operation> <number2>);
-  
+  console.log(`Question: ${number1} ${operation} ${number2}`);
 }
+
+startGame();
 
 export { startGame };
