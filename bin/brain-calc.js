45658d8b4e4a7120e5c51268112de7b2d2b51121
l1nky358 <prokotrob@gmail.com>
2025-11-11 17:14:36 +0700

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,38 +1,4 @@
-import { getRandomInt } from '../utils.js';
+#!/usr/bin/env node
+import { startGameCalc } from '../games/calc.js';
 
-const operators = ['+', '-', '*'];
-
-const generateExpression = () => {
-  const num1 = getRandomInt(1, 100);
-  const num2 = getRandomInt(1, 100);
-  const operator = operators[getRandomInt(0, operators.length - 1)];
-  const question = `${num1} ${operator} ${num2}`;
-  let answer;
-  switch (operator) {
-    case '+':
-      answer = num1 + num2;
-      break;
-    case '-':
-      answer = num1 - num2;
-      break;
-    case '*':
-      answer = num1 * num2;
-      break;
-  }
-  return { question, answer: answer.toString() };
-};
-
-const run = () => {
-  const { question, answer } = generateExpression();
-  console.log(`Question: ${question}`);
-  const userAnswer = prompt('Your answer:');
-  if (userAnswer === answer) {
-    console.log('Correct!');
-    return true;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-    return false;
-  }
-};
-
-export default { run };
+startGameCalc();
