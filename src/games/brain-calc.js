a8868153eff2726939f36cad9e99352b0418d261
l1nky358 <prokotrob@gmail.com>
2025-11-11 17:13:09 +0700

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,15 +1,35 @@
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const getQuestionAndAnswer = () => {
-  const operations = ['+', '-', '*'];
-  const operation = operations[getRandomInt(0, operations.length - 1)];
-  const num1 = getRandomInt(1, 100);
-  const num2 = getRandomInt(1, 100);
-  const question = `${num1} ${operation} ${num2}`;
+import { startGame } from '../src/index.js';
+
+const OPERATORS = ['+', '-', '*'];
+
+const getRandomOperator = () => OPERATORS[Math.floor(Math.random() * OPERATORS.length)];
+
+const generateRoundData = () => {
+  const num1 = Math.floor(Math.random() * 100) + 1;
+  const num2 = Math.floor(Math.random() * 100) + 1;
+  const operator = getRandomOperator();
+  const question = `${num1} ${operator} ${num2}`;
+
   let answer;
-
-  switch (operation) {
+  switch (operator) {
     case '+':
-
+      answer = num1 + num2;
+      break;
+    case '-':
+      answer = num1 - num2;
+      break;
+    case '*':
+      answer = num1 * num2;
+      break;
+    default:
+      answer = null; // на всякий случай
   }
-}
+
+  return { question, answer };
+};
+
+export const startGameCalc = () => {
+  const gameDescription = 'What is the result of the expression?';
+  startGame(gameDescription, generateRoundData);
+};
