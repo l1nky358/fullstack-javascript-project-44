12b19e95b06765ebfb4bee64340e4452103a4f1c
l1nky358 <prokotrob@gmail.com>
2025-11-13 13:39:48 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,1 @@
-export const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
 
-export const generateOperation = () => {
-  const operations = ['+', '-', '*'];
-  const index = Math.floor(Math.random() * operations.length);
-  return operations[index];
-};
