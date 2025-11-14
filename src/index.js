d9c313ab50658040e2d1eccc64a3bb9cdc1a2df2
l1nky358 <prokotrob@gmail.com>
2025-11-14 15:21:58 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import readline from 'readline-sync';
-import CalcGame from '../src/games/calc.js'; 
+import readlineSync from 'readline-sync';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+export const runGame = (description, getQuestionAndAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(description);
 
-console.log('Welcome to the Brain Games!');
-rl.question('May I have your name? ', (name) => {
-    console.log(`Hello, ${name}!`);
-    console.log('What is the result of the expression?');
-
-    brainCalcGame.startBrainCalc(name); 
-});
+  const maxRounds = 3;
+  for (let i = 0; i < maxRounds; i += 1) {
+    const { question, answer } = getQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (parseInt(userAnswer, 10) !== answer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
