f51e4c2361bccb55a025b4eb42434735248c7253
l1nky358 <prokotrob@gmail.com>
2025-11-14 19:10:47 +0700

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import playBrainCalc from '../src/games/brain-calc.js';
+import playBrainCalc from '../src/games/calc.js';
 
 playBrainCalc(readlineSync);
