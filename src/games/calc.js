aa09dc454d76f4b99f260a9f91b805936e3ffc0d
l1nky358 <prokotrob@gmail.com>
2025-11-14 15:22:31 +0700

    Update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,67 +1,13 @@
-import readline from 'readline-sync';
+import { runGame } from '../src/index.js';
 
-function generateExpression() {
-    const num1 = Math.floor(Math.random() * 100);
-    const num2 = Math.floor(Math.random() * 100);
-    const operators = ['+', '-', '*'];
-    const operator = operators[Math.floor(Math.random() * operators.length)];
-    
-    return {num1, num2, operator};
-}
-
-function checkAnswer(answer, correctAnswer) {
-    if (answer === correctAnswer.toString()) {
-        console.log("Correct!");
-        return true;
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        return false;
-    }
-}
-
-export default function startBrainCalc(name) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    let roundCount = 0;
-    const maxRounds = 3;
-
-    function playRound() {
-        const exprObj = generateExpression();
-        const question = `${exprObj.num1} ${exprObj.operator} ${exprObj.num2}`;
-        
-        let correctAnswer;
-        switch(exprObj.operator) {
-            case '+':
-                correctAnswer = exprObj.num1 + exprObj.num2;
-                break;
-            case '-':
-                correctAnswer = exprObj.num1 - exprObj.num2;
-                break;
-            case '*':
-                correctAnswer = exprObj.num1 * exprObj.num2;
-                break;
-        }
-
-        console.log(`Question: ${question}`);
-        rl.question("Your answer: ", (input) => {
-            if (!checkAnswer(input.trim(), correctAnswer)) {
-                console.log(`Let's try again, ${name}!`);
-                rl.close();
-            } else {
-                roundCount++;
-                if (roundCount >= maxRounds) {
-                    console.log(`Congratulations, ${name}!`);
-                    rl.close();
-                } else {
-                    playRound(); 
-                }
-            }
-        });
-    }
-
-    playRound(); 
-};
-
+const getQuestionAndAnswer = () => {
+  const operations = ['+', '-', '*'];
+  const number1 = Math.floor(Math.random() * 100);
+  const number2 = Math.floor(Math.random() * 100);
+  const operation = operations[Math.floor(Math.random() * operations.length)];
+  const question = `${number1} ${operation} ${number2}`;
+  let answer;
+  switch (operation) {
+    case '+':
+      answer = number1 + number2;
+      break;
