6f8166daaf5c1cae8e868563a1a66b2829366322
l1nky358 <prokotrob@gmail.com>
2025-11-14 20:00:35 +0700

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-console.log('node bin/brain-games.js');
-console.log('Welcome to the Brain Games!');
-import { greetUser } from '../src/cli.js';
+import { greetUser } from '../src/cli.js'
 
-greetUser();
+console.log('node bin/brain-games.js')
+console.log('Welcome to the Brain Games!')
+
+greetUser()
