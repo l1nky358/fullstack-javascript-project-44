3e82dc38e731149c618e41865f0b79ad39d894b0
l1nky358 <prokotrob@gmail.com>
2025-11-17 13:53:49 +0700

    Update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,45 +1,45 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-const operators = ['+', '-', '*'];
+const operators = ['+', '-', '*']
 
 const generateExpression = () => {
-  const num1 = Math.floor(Math.random() * 100) + 1;
-  const num2 = Math.floor(Math.random() * 100) + 1;
-  const operator = operators[Math.floor(Math.random() * operators.length)];
-  const question = `${num1} ${operator} ${num2}`;
-  let answer;
+  const num1 = Math.floor(Math.random() * 100) + 1
+  const num2 = Math.floor(Math.random() * 100) + 1
+  const operator = operators[Math.floor(Math.random() * operators.length)]
+  const question = `${num1} ${operator} ${num2}`
+  let answer
   switch (operator) {
     case '+':
-      answer = num1 + num2;
-      break;
+      answer = num1 + num2
+      break
     case '-':
-      answer = num1 - num2;
-      break;
+      answer = num1 - num2
+      break
     case '*':
-      answer = num1 * num2;
-      break;
+      answer = num1 * num2
+      break
   }
-  return { question, answer: answer.toString() };
-};
+  return { question, answer: answer.toString() }
+}
 
 const playBrainCalc = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('What is the result of the expression?');
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+  console.log('What is the result of the expression?')
 
-  const rounds = 3;
+  const rounds = 3
   for (let i = 0; i < rounds; i++) {
-    const { question, answer } = generateExpression();
-    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    const { question, answer } = generateExpression()
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `)
     if (userAnswer === answer) {
-      console.log('Correct!');
+      console.log('Correct!')
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
     }
   }
-  console.log(`Congratulations, ${name}!`);
-};
+  console.log(`Congratulations, ${name}!`)
+}
 
-export default playBrainCalc;
+export default playBrainCalc
