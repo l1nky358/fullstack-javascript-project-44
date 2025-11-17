2e39050863a789e2e50565d25aa01280708a4da4
l1nky358 <prokotrob@gmail.com>
2025-11-17 14:04:01 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export const runGame = (description, getQuestionAndAnswer) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(description);
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log(description)
 
-  const maxRounds = 3;
+  const maxRounds = 3
   for (let i = 0; i < maxRounds; i += 1) {
-    const { question, answer } = getQuestionAndAnswer();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const { question, answer } = getQuestionAndAnswer()
+    console.log(`Question: ${question}`)
+    const userAnswer = readlineSync.question('Your answer: ')
     if (parseInt(userAnswer, 10) !== answer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`)
+      console.log(`Let's try again, ${userName}!`)
+      return
     }
-    console.log('Correct!');
+    console.log('Correct!')
   }
-  console.log(`Congratulations, ${userName}!`);
-};
+  console.log(`Congratulations, ${userName}!`)
+}
