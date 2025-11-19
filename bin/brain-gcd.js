14771d0534a11579292dd218a5e5606e2ec512fc
l1nky358 <prokotrob@gmail.com>
2025-11-19 14:41:34 +0700

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -36,8 +36,7 @@ const runGame = () => {
     if (userAnswer === correctAnswer) {
       console.log('Correct!')
     }
-    else 
-    {
+    else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
       console.log(`Let's try again, ${name}!`)
       return
